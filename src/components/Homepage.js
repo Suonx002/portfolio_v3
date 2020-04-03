c06f946e28080f6a19dbd2100e8226d58b25d930
Vuthy <suonx002@gmail.com>
2020-04-03 13:56:14 -0500

    change engineer to developer

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -36,7 +36,7 @@ const Homepage = () => {
             </Grid>
             <Grid item className={classes.gridItemMarginBottom}>
               <Typography variant='h3' className={classes.infoText}>
-                a software engineer based in Minneapolis, MN.
+                a software developer based in Minneapolis, MN.
               </Typography>
             </Grid>
             <Grid item className={classes.gridItemMarginBottom}>
